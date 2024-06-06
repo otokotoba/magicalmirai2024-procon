603d0bde85db990d6c4cdbffe80766d4f36fd9d7
otokotoba <otokotoba.music@gmail.com>
2024-06-07 02:38:12 +0900

    add sections to display a time progress text in the player controls

--- src/components/TextAlivePlayerControls.tsx
+++ src/components/TextAlivePlayerControls.tsx
@@ -5,9 +5,13 @@ import {
   AppBar,
   Box,
   CircularProgress,
+  Grid,
   IconButton,
   Slider,
+  SliderValueLabelProps,
   Toolbar,
+  Tooltip,
+  Typography,
 } from '@mui/material';
 import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { Player } from 'textalive-app-api';
@@ -53,35 +57,93 @@ export function TextAlivePlayerControls({
     [player, setProgress]
   );
 
+  const formatSliderValue = useCallback(
+    (value: number) => {
+      if (!player) return;
+
+      const ms = player.video.duration * (value / 100);
+      const s = Math.trunc((ms / 1000) % 60);
+      const m = Math.trunc(ms / 1000 / 60);
+
+      const pad = (i: number): string => {
+        if (i < 10) return `0${i}`;
+        else return `${i}`;
+      };
+
+      return `${pad(m)}:${pad(s)}`;
+    },
+    [player]
+  );
+
   return (
     <Box>
       <AppBar position="fixed" color="info" sx={{ top: 'auto', buttom: 0 }}>
-        <Toolbar>
-          <Box sx={{ position: 'relative' }}>
-            {loading && (
-              <CircularProgress color="inherit" sx={{ position: 'absolute' }} />
-            )}
-            <IconButton
-              disabled={loading}
-              color="inherit"
-              onClick={handleClick}
-              sx={{ mr: 2 }}
-            >
-              {!playing || progress === 100 ? (
-                <PlayArrowRounded />
-              ) : (
-                <StopRounded />
+        <Toolbar sx={{ py: 1 }} variant="dense">
+          <Grid container alignItems="center">
+            <Grid item xs={12}>
+              <Slider
+                disabled={loading}
+                value={progress}
+                sx={{
+                  color: '#fff',
+                  ml: 1,
+                  '& .MuiSlider-rail': {
+                    width: 'calc(100% - 8px)',
+                  },
+                }}
+                size="small"
+                onChange={handleChangeProgress}
+                slots={{
+                  valueLabel: SliderValueLabel,
+                }}
+                valueLabelDisplay="auto"
+                valueLabelFormat={formatSliderValue}
+              />
+            </Grid>
+
+            <Grid item>
+              <Box sx={{ position: 'relative' }}>
+                {loading && (
+                  <CircularProgress
+                    color="inherit"
+                    sx={{ position: 'absolute' }}
+                  />
+                )}
+                <IconButton
+                  disabled={loading}
+                  color="inherit"
+                  onClick={handleClick}
+                >
+                  {!playing || progress === 100 ? (
+                    <PlayArrowRounded />
+                  ) : (
+                    <StopRounded />
+                  )}
+                </IconButton>
+              </Box>
+            </Grid>
+
+            <Grid item mx={1}>
+              {!loading && (
+                <Typography variant="caption">
+                  {`${formatSliderValue(progress)} / ${formatSliderValue(100)}`}
+                </Typography>
               )}
-            </IconButton>
-          </Box>
-          <Slider
-            disabled={loading}
-            value={progress}
-            sx={{ color: '#fff', mr: 1 }}
-            onChange={handleChangeProgress}
-          />
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </Box>
+  );
+}
+
+function SliderValueLabel({
+  children,
+  value,
+}: SliderValueLabelProps): JSX.Element {
+  return (
+    <Tooltip enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
   );
 }
