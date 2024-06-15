a1226308af7a5f5b409b2c2b0046d600b11e59b5
otokotoba <otokotoba.music@gmail.com>
2024-06-15 18:01:06 +0900

    update the Player component with a character controller library

--- src/components/TextAlivePlayerGame.tsx
+++ src/components/TextAlivePlayerGame.tsx
@@ -1,59 +1,57 @@
-import { KeyboardControls, PointerLockControls, Sky } from '@react-three/drei';
+import { PointerLockControls, Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/rapier';
 import { Suspense } from 'react';
 import { VSMShadowMap } from 'three';
 
-import { KEYMAP, Player } from '@/components/game/Player';
+import { Player } from '@/components/game/Player';
 import { World } from '@/components/game/World';
 
 export function TextAlivePlayerGame(): JSX.Element {
   return (
-    <KeyboardControls map={KEYMAP}>
-      <Canvas
-        shadows
-        camera={{ fov: 70, near: 0.1, far: 1000 }}
-        resize={{ debounce: 50 }}
-        onCreated={state => {
-          state.gl.shadowMap.type = VSMShadowMap;
-          state.gl.setPixelRatio(window?.devicePixelRatio);
-        }}
+    <Canvas
+      shadows
+      camera={{ fov: 70, near: 0.1, far: 1000 }}
+      resize={{ debounce: 50 }}
+      onCreated={state => {
+        state.gl.shadowMap.type = VSMShadowMap;
+        state.gl.setPixelRatio(window?.devicePixelRatio);
+      }}
+    >
+      <Sky sunPosition={[0, 10, 0]} />
+      <ambientLight intensity={0.2} />
+      <directionalLight
+        color={0xffffff}
+        intensity={2}
+        position={[0, 20, 0]}
+        castShadow
+        shadow-mapSize={[1024, 1024]}
+        shadow-radius={4}
+        shadow-bias={-0.0001}
       >
-        <Sky sunPosition={[0, 10, 0]} />
-        <ambientLight intensity={0.2} />
-        <directionalLight
-          color={0xffffff}
-          intensity={2}
-          position={[0, 20, 0]}
-          castShadow
-          shadow-mapSize={[1024, 1024]}
-          shadow-radius={4}
-          shadow-bias={-0.0001}
-        >
-          <orthographicCamera
-            attach={'shadow-camera'}
-            left={-30}
-            right={30}
-            top={30}
-            bottom={-30}
-          />
-        </directionalLight>
-        <hemisphereLight
-          color={0x8dc1de}
-          groundColor={0x00668d}
-          intensity={1.5}
-          position={[0, 1, 0]}
+        <orthographicCamera
+          attach={'shadow-camera'}
+          left={-30}
+          right={30}
+          top={30}
+          bottom={-30}
         />
+      </directionalLight>
+      <hemisphereLight
+        color={0x8dc1de}
+        groundColor={0x00668d}
+        intensity={1.5}
+        position={[0, 1, 0]}
+      />
 
-        <Suspense>
-          <Physics gravity={[0, -30, 0]} colliders={false}>
-            <World />
-            <Player />
-          </Physics>
-        </Suspense>
+      <Suspense>
+        <Physics gravity={[0, -30, 0]} colliders={false}>
+          <World />
+          <Player />
+        </Physics>
+      </Suspense>
 
-        <PointerLockControls />
-      </Canvas>
-    </KeyboardControls>
+      <PointerLockControls />
+    </Canvas>
   );
 }
--- src/components/game/Player.tsx
+++ src/components/game/Player.tsx
@@ -1,77 +1,29 @@
-import { Ray } from '@dimforge/rapier3d-compat';
-import { KeyboardControlsEntry, useKeyboardControls } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
-import {
-  CapsuleArgs,
-  CapsuleCollider,
-  RapierRigidBody,
-  RigidBody,
-  useRapier,
-} from '@react-three/rapier';
-import { useRef } from 'react';
-import { Vector3 } from 'three';
+import { KeyboardControls, KeyboardControlsEntry } from '@react-three/drei';
+import Ecctrl from 'ecctrl';
 
-enum Commands {
-  forward = 'forward',
-  backward = 'backward',
-  left = 'left',
-  right = 'right',
-  jump = 'jump',
-}
-
-export const KEYMAP: KeyboardControlsEntry<Commands>[] = [
-  { name: Commands.forward, keys: ['ArrowUp', 'KeyW'] },
-  { name: Commands.backward, keys: ['ArrowDown', 'KeyS'] },
-  { name: Commands.left, keys: ['ArrowLeft', 'KeyA'] },
-  { name: Commands.right, keys: ['ArrowRight', 'KeyD'] },
-  { name: Commands.jump, keys: ['Space'] },
+export const KEYMAP: KeyboardControlsEntry[] = [
+  { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+  { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+  { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+  { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+  { name: 'jump', keys: ['Space'] },
+  { name: 'run', keys: ['Shift'] },
 ];
 
-const SPEED = 5;
-const direction = new Vector3();
-const frontVector = new Vector3();
-const sideVector = new Vector3();
-const capsuleArgs: CapsuleArgs = [0.5, 0.25];
-
 export function Player(): JSX.Element {
-  const ref = useRef<RapierRigidBody | null>(null);
-  const { world } = useRapier();
-  const [, get] = useKeyboardControls<Commands>();
-
-  useFrame(state => {
-    if (!ref.current) return;
-    const { forward, backward, left, right, jump } = get();
-    const vel = ref.current.linvel();
-
-    state.camera.position.copy(ref.current.translation());
-
-    frontVector.set(0, 0, Number(backward) - Number(forward));
-    sideVector.set(Number(left) - Number(right), 0, 0);
-    direction
-      .subVectors(frontVector, sideVector)
-      .normalize()
-      .multiplyScalar(SPEED)
-      .applyEuler(state.camera.rotation);
-    ref.current.setLinvel({ x: direction.x, y: vel.y, z: direction.z }, true);
-
-    // eslint-disable-next-line @react-three/no-new-in-loop
-    const ray = new Ray(ref.current.translation(), { x: 0, y: -1, z: 0 });
-    const hit = world.castRay(ray, 1, false);
-    const grounded =
-      hit && hit.collider && hit.toi < capsuleArgs[0] + capsuleArgs[1];
-    if (jump && grounded) ref.current.setLinvel({ x: 0, y: 7.5, z: 0 }, true);
-  });
-
   return (
-    <RigidBody
-      ref={ref}
-      colliders={false}
-      mass={1}
-      type="dynamic"
-      position={[0, 10, 0]}
-      enabledRotations={[false, false, false]}
-    >
-      <CapsuleCollider args={capsuleArgs} />
-    </RigidBody>
+    <KeyboardControls map={KEYMAP}>
+      <Ecctrl
+        maxVelLimit={4}
+        jumpVel={5}
+        // First-person view
+        camInitDis={-0.01}
+        camMinDis={-0.01}
+        camFollowMult={100}
+        turnVelMultiplier={1}
+        turnSpeed={100}
+        mode="CameraBasedMovement"
+      />
+    </KeyboardControls>
   );
 }
