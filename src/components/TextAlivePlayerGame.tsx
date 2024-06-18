bad442c49bd1510bf2710b5687d3176a0b0673ff
otokotoba <otokotoba.music@gmail.com>
2024-06-19 01:33:58 +0900

    change the lights & background in the scene to ones for night

--- src/components/TextAlivePlayerGame.tsx
+++ src/components/TextAlivePlayerGame.tsx
@@ -1,8 +1,8 @@
-import { Center, PointerLockControls, Sky } from '@react-three/drei';
+import { Center, PointerLockControls, Stars } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/rapier';
 import { Suspense, useCallback, useEffect, useRef } from 'react';
-import { VSMShadowMap } from 'three';
+import { Color, VSMShadowMap } from 'three';
 
 import { useAppStore } from '@/components/AppStoreProvider';
 import { Player } from '@/components/game/Player';
@@ -51,6 +51,7 @@ export function TextAlivePlayerGame(): JSX.Element {
     <Canvas
       ref={canvasRef}
       shadows
+      scene={{ background: new Color(0x000b1a) }}
       camera={{ fov: 70, near: 0.1, far: 1000 }}
       resize={{ debounce: 50 }}
       onCreated={state => {
@@ -58,12 +59,12 @@ export function TextAlivePlayerGame(): JSX.Element {
         state.gl.setPixelRatio(window?.devicePixelRatio);
       }}
     >
-      <Sky sunPosition={[0, 10, 0]} />
-      <ambientLight intensity={0.2} />
+      <Stars radius={400} />
+      <ambientLight intensity={0.5} />
       <directionalLight
         color={0xffffff}
         intensity={2}
-        position={[0, 20, 0]}
+        position={[0, 50, 0]}
         castShadow
         shadow-mapSize={[1024, 1024]}
         shadow-radius={4}
@@ -78,10 +79,10 @@ export function TextAlivePlayerGame(): JSX.Element {
         />
       </directionalLight>
       <hemisphereLight
-        color={0x8dc1de}
-        groundColor={0x00668d}
+        color={0xbdbdbd}
+        groundColor={0x000000}
         intensity={1.5}
-        position={[0, 1, 0]}
+        position={[0, 0, 0]}
       />
       <Suspense>
         <Physics gravity={[0, -30, 0]} colliders={false}>
