83918affb70b64bf95c4ac846288208d282a7829
otokotoba <otokotoba.music@gmail.com>
2024-06-20 18:32:54 +0900

    simple fix for production build

--- src/components/game/Stage.tsx
+++ src/components/game/Stage.tsx
@@ -1,15 +1,17 @@
 import { useGLTF } from '@react-three/drei';
 import { RigidBody } from '@react-three/rapier';
-import { Mesh, MeshStandardMaterial } from 'three';
+import { InstancedBufferAttribute, Mesh, MeshStandardMaterial } from 'three';
 import { GLTF } from 'three-stdlib';
+
+type InstancedMesh = Mesh & { instanceMatrix: InstancedBufferAttribute };
 
 type GLTFResult = GLTF & {
   nodes: {
+    StaticMeshActor_0_StaticMeshComponent0: Mesh;
     StaticMeshActor_1_StaticMeshComponent0: Mesh;
     StaticMeshActor_5_StaticMeshComponent0: Mesh;
     StaticMeshActor_10_StaticMeshComponent0: Mesh;
     StaticMeshActor_17_StaticMeshComponent0: Mesh;
-    StaticMeshActor_19_StaticMeshComponent0: Mesh;
     StaticMeshActor_20_StaticMeshComponent0: Mesh;
     StaticMeshActor_36_StaticMeshComponent0: Mesh;
     StaticMeshActor_43_StaticMeshComponent0: Mesh;
@@ -23,18 +25,18 @@ type GLTFResult = GLTF & {
     StaticMeshActor_103_StaticMeshComponent0: Mesh;
     StaticMeshActor_105_StaticMeshComponent0: Mesh;
     StaticMeshActor_107_StaticMeshComponent0: Mesh;
-    SM_Yn: Mesh;
-    SM_Yn_1: Mesh;
-    SM_Robot_Wash_Head: Mesh;
-    SM_Robot_Wash_Head_1: Mesh;
-    SM_LX_17001: Mesh;
-    SM_LX_17: Mesh;
+    SM_LX_17: InstancedMesh;
+    SM_Yn: InstancedMesh;
+    SM_Yn_1: InstancedMesh;
+    SM_Robot_Wash_Head: InstancedMesh;
+    SM_Robot_Wash_Head_1: InstancedMesh;
+    SM_LX_17001: InstancedMesh;
   };
   materials: {
-    M_Film: MeshStandardMaterial;
     PaletteMaterial001: MeshStandardMaterial;
+    M_Film: MeshStandardMaterial;
+    PaletteMaterial002: MeshStandardMaterial;
     MI_Nightmare: MeshStandardMaterial;
-    PaletteMaterial003: MeshStandardMaterial;
     PaletteMaterial004: MeshStandardMaterial;
     M_Zd: MeshStandardMaterial;
     PaletteMaterial006: MeshStandardMaterial;
@@ -42,16 +44,16 @@ type GLTFResult = GLTF & {
     M_FG01: MeshStandardMaterial;
     MI_Steel_se2abd3c_2K: MeshStandardMaterial;
     PaletteMaterial007: MeshStandardMaterial;
-    PaletteMaterial002: MeshStandardMaterial;
+    PaletteMaterial003: MeshStandardMaterial;
     PaletteMaterial008: MeshStandardMaterial;
     M_Stage_ACCs_MetalMaster: MeshStandardMaterial;
     MI_Nightmare_2: MeshStandardMaterial;
     M_TX_02: MeshStandardMaterial;
     M_TX_03: MeshStandardMaterial;
     M_TX_01: MeshStandardMaterial;
+    MI_Stage_ACCs_Metal_Trusses: MeshStandardMaterial;
     MI_Light_Robot_Wash: MeshStandardMaterial;
     PaletteMaterial005: MeshStandardMaterial;
-    MI_Stage_ACCs_Metal_Trusses: MeshStandardMaterial;
   };
 };
 
@@ -61,114 +63,109 @@ export function Stage(props: JSX.IntrinsicElements['group']): JSX.Element {
     <RigidBody type="fixed" colliders="trimesh">
       <group {...props} dispose={null}>
         <mesh
+          geometry={nodes.StaticMeshActor_0_StaticMeshComponent0.geometry}
+          material={materials.PaletteMaterial001}
+          position={[1.228, -8.632, 0.053]}
+          scale={[200, 113.44, 113.44]}
+        />
+        <mesh
           geometry={nodes.StaticMeshActor_1_StaticMeshComponent0.geometry}
           material={materials.M_Film}
-          position={[5.333, -2.952, -3.012]}
+          position={[5.723, -2.952, 0.288]}
           rotation={[Math.PI / 2, 0, Math.PI / 2]}
           scale={[9.807, 1, 5.359]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_5_StaticMeshComponent0.geometry}
-          material={materials.PaletteMaterial001}
-          position={[-9.493, -8.159, -8.649]}
+          material={materials.PaletteMaterial002}
+          position={[-9.103, -8.159, -5.349]}
           rotation={[0, -0.611, 0]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_10_StaticMeshComponent0.geometry}
           material={materials.MI_Nightmare}
-          position={[-0.463, 13.107, -3.2]}
+          position={[-0.073, 13.107, 0.1]}
           scale={[1.776, 12.3, 1.776]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_17_StaticMeshComponent0.geometry}
-          material={materials.PaletteMaterial003}
-          position={[-9.493, -7.725, -8.649]}
-          rotation={[0, -0.611, -0.96]}
-        />
-        <mesh
-          geometry={nodes.StaticMeshActor_19_StaticMeshComponent0.geometry}
           material={materials.PaletteMaterial004}
-          position={[21.681, -7.374, -3.2]}
-          rotation={[0, -Math.PI / 2, 0]}
-          scale={[1, 1.465, 1]}
+          position={[-9.103, -7.725, -5.349]}
+          rotation={[0, -0.611, -0.96]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_20_StaticMeshComponent0.geometry}
           material={materials.M_Zd}
-          position={[20.311, -4.431, -3.2]}
+          position={[20.701, -4.431, 0.1]}
           rotation={[0, -Math.PI / 2, 0]}
           scale={[0.941, 0.707, 1.19]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_36_StaticMeshComponent0.geometry}
           material={materials.PaletteMaterial006}
-          position={[0.605, -8.989, -3.2]}
+          position={[0.995, -8.989, 0.1]}
           rotation={[Math.PI / 2, -1.484, Math.PI / 2]}
           scale={2.3}
         />
         <mesh
           geometry={nodes.StaticMeshActor_43_StaticMeshComponent0.geometry}
           material={materials.M_FG_05}
-          position={[1.863, -2.794, -2.315]}
+          position={[2.253, -2.794, 0.985]}
           scale={3.616}
         />
         <mesh
           geometry={nodes.StaticMeshActor_55_StaticMeshComponent0.geometry}
           material={materials.M_FG01}
-          position={[0.067, -8.541, -3.2]}
+          position={[0.457, -8.541, 0.1]}
           rotation={[0, -Math.PI / 2, 0]}
           scale={[3.65, 2.903, 3.65]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_57_StaticMeshComponent0.geometry}
           material={materials.MI_Steel_se2abd3c_2K}
-          position={[-0.898, -7.075, -0.287]}
+          position={[-0.508, -7.075, 3.013]}
           scale={[0.737, 0.918, 0.721]}
         />
-        <group position={[-4.805, -7.488, -5.84]}>
+        <group position={[-4.415, -7.488, -2.54]}>
           <mesh
             geometry={nodes.SM_MERGED_StaticMeshActor_89.geometry}
             material={materials.PaletteMaterial007}
           />
           <mesh
             geometry={nodes.SM_MERGED_StaticMeshActor_89_1.geometry}
-            material={materials.PaletteMaterial002}
+            material={materials.PaletteMaterial003}
           />
         </group>
         <mesh
           geometry={nodes.StaticMeshActor_81_StaticMeshComponent0.geometry}
           material={materials.PaletteMaterial008}
-          position={[-3.372, -7.423, -7.263]}
+          position={[-2.982, -7.423, -3.963]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_95_StaticMeshComponent0.geometry}
           material={materials.M_Stage_ACCs_MetalMaster}
-          position={[-4.725, -7.652, -0.435]}
+          position={[-4.335, -7.652, 2.865]}
           rotation={[0, -Math.PI / 2, 0]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_101_StaticMeshComponent0.geometry}
           material={materials.MI_Nightmare_2}
-          position={[0, -7.691, -3.2]}
+          position={[0.39, -7.691, 0.1]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_103_StaticMeshComponent0.geometry}
           material={materials.M_TX_02}
-          position={[1.554, -5.86, -7.914]}
+          position={[1.944, -5.86, -4.614]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_105_StaticMeshComponent0.geometry}
           material={materials.M_TX_03}
-          position={[2.053, -6.436, 2.759]}
+          position={[2.443, -6.436, 6.059]}
         />
         <mesh
           geometry={nodes.StaticMeshActor_107_StaticMeshComponent0.geometry}
           material={materials.M_TX_01}
-          position={[-1.058, -6.338, -9.808]}
-        />
-        <instancedMesh
-          args={[nodes.SM_LX_17001.geometry, materials.PaletteMaterial001, 5]}
-          instanceMatrix={nodes.SM_LX_17001.instanceMatrix}
+          position={[-0.668, -6.338, -6.508]}
         />
         <instancedMesh
           args={[
@@ -179,11 +176,15 @@ export function Stage(props: JSX.IntrinsicElements['group']): JSX.Element {
           instanceMatrix={nodes.SM_LX_17.instanceMatrix}
         />
         <instancedMesh
-          args={[nodes.SM_Yn.geometry, materials.PaletteMaterial001, 5]}
+          args={[nodes.SM_LX_17001.geometry, materials.PaletteMaterial002, 5]}
+          instanceMatrix={nodes.SM_LX_17001.instanceMatrix}
+        />
+        <instancedMesh
+          args={[nodes.SM_Yn.geometry, materials.PaletteMaterial002, 5]}
           instanceMatrix={nodes.SM_Yn.instanceMatrix}
         />
         <instancedMesh
-          args={[nodes.SM_Yn_1.geometry, materials.PaletteMaterial002, 5]}
+          args={[nodes.SM_Yn_1.geometry, materials.PaletteMaterial003, 5]}
           instanceMatrix={nodes.SM_Yn_1.instanceMatrix}
         />
         <instancedMesh
