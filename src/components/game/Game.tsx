2fddd879257506d648849ccd579544671ea9bf5b
otokotoba <otokotoba.music@gmail.com>
2024-06-26 00:55:55 +0900

    add a loading overlay to the Game component

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,4 +1,4 @@
-import { Center, PointerLockControls, Stars } from '@react-three/drei';
+import { Center, Loader, PointerLockControls, Stars } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/rapier';
 import { Suspense, useCallback, useEffect, useRef } from 'react';
@@ -51,67 +51,71 @@ export function Game(): JSX.Element {
   const alive = useAppStore(state => state.alive);
 
   return (
-    <Canvas
-      ref={canvasRef}
-      shadows
-      scene={{ background: new Color(0x000b1a) }}
-      camera={{ fov: 70, near: 0.1, far: 1000 }}
-      resize={{ debounce: 50 }}
-      onCreated={state => {
-        state.gl.shadowMap.type = VSMShadowMap;
-        state.gl.setPixelRatio(window?.devicePixelRatio);
-      }}
-    >
-      <Stars radius={400} />
-      <fog attach="fog" args={[0x000000, 0, 100]} />
-      <ambientLight intensity={0.5} />
-      <directionalLight
-        color={0xffffff}
-        intensity={2}
-        position={[0, 50, 0]}
-        castShadow
-        shadow-mapSize={[1024, 1024]}
-        shadow-radius={4}
-        shadow-bias={-0.0001}
+    <>
+      <Canvas
+        ref={canvasRef}
+        shadows
+        scene={{ background: new Color(0x000b1a) }}
+        camera={{ fov: 70, near: 0.1, far: 1000 }}
+        resize={{ debounce: 50 }}
+        onCreated={state => {
+          state.gl.shadowMap.type = VSMShadowMap;
+          state.gl.setPixelRatio(window?.devicePixelRatio);
+        }}
       >
-        <orthographicCamera
-          attach={'shadow-camera'}
-          left={-30}
-          right={30}
-          top={30}
-          bottom={-30}
+        <Stars radius={400} />
+        <fog attach="fog" args={[0x000000, 0, 100]} />
+        <ambientLight intensity={0.5} />
+        <directionalLight
+          color={0xffffff}
+          intensity={2}
+          position={[0, 50, 0]}
+          castShadow
+          shadow-mapSize={[1024, 1024]}
+          shadow-radius={4}
+          shadow-bias={-0.0001}
+        >
+          <orthographicCamera
+            attach={'shadow-camera'}
+            left={-30}
+            right={30}
+            top={30}
+            bottom={-30}
+          />
+        </directionalLight>
+        <hemisphereLight
+          color={0xbdbdbd}
+          groundColor={0x000000}
+          intensity={1.5}
+          position={[0, 0, 0]}
         />
-      </directionalLight>
-      <hemisphereLight
-        color={0xbdbdbd}
-        groundColor={0x000000}
-        intensity={1.5}
-        position={[0, 0, 0]}
-      />
-      <Suspense>
-        <Physics gravity={[0, -30, 0]} colliders={false}>
-          <Center>
-            <Stage />
-            <RacingMiku
-              position={[0, -8, 0]}
-              scale={0.2}
-              rotation={[0, -Math.PI / 2, 0]}
-            />
-            {alive && (
-              <Player
-                position={[-25, 0, 0]}
-                camInitDir={{ x: 0, y: Math.PI / 2 }}
+        <Suspense>
+          <Physics gravity={[0, -30, 0]} colliders={false}>
+            <Center>
+              <Stage />
+              <RacingMiku
+                position={[0, -8, 0]}
+                scale={0.2}
+                rotation={[0, -Math.PI / 2, 0]}
               />
-            )}
-          </Center>
-        </Physics>
-      </Suspense>
+              {alive && (
+                <Player
+                  position={[-25, 0, 0]}
+                  camInitDir={{ x: 0, y: Math.PI / 2 }}
+                />
+              )}
+            </Center>
+          </Physics>
+        </Suspense>
 
-      <PointerLockControls
-        selector="#game"
-        onLock={handleLock}
-        onUnlock={handleUnLock}
-      />
-    </Canvas>
+        <PointerLockControls
+          selector="#game"
+          onLock={handleLock}
+          onUnlock={handleUnLock}
+        />
+      </Canvas>
+
+      <Loader />
+    </>
   );
 }
